1391769b9a1b26a0bdde4675a2829d2abe49720c
Houra-Am <61083815+Houra-Am@users.noreply.github.com>
2021-01-12 15:10:00 +0100

    exo23

--- exercice23/js/main.js
+++ exercice23/js/main.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
     $("button").click(function() {
-        $("#square").animate({
+        /*$("#square").animate({
                 "bottom": "-=110px",
                 "left": "+=960px"
             },
-            "slow")
+            "slow")*/
+
+        $("#square").animate({
+            'bottom': "0px",
+            'right': "0px",
+        });
 
     });
 });
