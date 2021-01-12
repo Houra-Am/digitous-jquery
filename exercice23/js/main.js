de7e76a3f8703fac4dc5988eacfe9b36bfc8584b
Houra-Am <61083815+Houra-Am@users.noreply.github.com>
2021-01-12 08:44:43 +0100

    exo23

---
+++ exercice23/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("button").click(function() {
+        $("#square").animate({
+                "bottom": "-=110px",
+                "right": "-=960px"
+
+            },
+
+            "slow")
+
+
+    });
+});
